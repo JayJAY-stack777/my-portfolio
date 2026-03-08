59c7dc8c09d7dcb9e1ca1b603d83252e91323fd2
Ntsika Tshuma <ntsika.tshuma.sa@gmail.com>
2026-03-08 21:20:15 +0200

    form

--- app/pages/contact.vue
+++ app/pages/contact.vue
@@ -1,39 +1,26 @@
 <template>
+  
+    <h1>Contact Me</h1>
 
-<h1>Contact Me</h1>
 
-<form 
-  name="contact"
-  action="POST"
-  data-netlify="true"
-  netlify
-  netlify-honeypot="bot-field"
-  class="form"
->
+<form name="contact" method="POST" data-netlify="true" class="form">
+      <input type="hidden" name="form-name" value="contact" />
 
-<input type="hidden" name="form-name" value="contact">
+      <label>Name:</label>
+      <input type="text" name="name" required />
+        <br>
+        <br>
+      <label>Email:</label>
+      <input type="email" name="email" required />
+        <br>
+        <br>
+      <label>Message:</label>
+      <textarea name="message" required></textarea>
 
-<p hidden>
-  <label>Don't fill this out: <input name="bot-field"></label>
-</p>
-
-<label>Name:</label>
-<input type="text" name="name" required />
-
-<br><br>
-
-<label>Email:</label>
-<input type="email" name="email" required />
-
-<br><br>
-
-<label>Message:</label>
-<textarea name="message" required></textarea>
-
-<button type="submit">Send</button>
-
-</form>
-
+      <button type="submit">Send</button>
+    
+    </form>
+ 
 </template>
 <style>
 .form {
